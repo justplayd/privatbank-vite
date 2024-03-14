8000d08f3922d6a3c0b73400ec65a947854a8118
viktor <justplayd@ukr.net>
2024-03-14 12:48:32 +0200

    config: add src/config

---
+++ src/config/config.ts
@@ -0,0 +1,6 @@
+// export const NODE_ENV = process.env.NODE_ENV;
+// export const IS_PRODUCTION = NODE_ENV === 'production';
+
+// export const APP_URL = process.env.REACT_APP_APP_URL;
+// export const API_REST_URL = process.env.REACT_APP_API_REST_URL;
+// export const API_GRAPHQL_URL = process.env.REACT_APP_API_GRAPHQL_URL;
